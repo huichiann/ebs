bc340710501a044566178c6bb6326b8158e51839
huichiann <hcboon.2017@sis.smu.edu.sg>
2020-11-10 20:45:44 +0800

    add time

--- src/components/MakeAppt.vue
+++ src/components/MakeAppt.vue
@@ -61,31 +61,62 @@
                 :items="items"
                 label="Select your clinic"
             ></v-select>
-            <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-            >
-                <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                    v-model="date"
-                    label="Date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                ></v-text-field>
-                </template>
-                <v-date-picker
-                v-model="date"
-                no-title
-                scrollable
-                @input="menu=false"
-                ></v-date-picker>
-            </v-menu>
-            <div style="width:200px;" class="mx-auto">
+            <v-row class="ma-0 pa-0">
+                <v-col cols="6" class="ma-0 pa-0"> 
+                    <v-menu
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                            class="ma-0 pa-0"
+                            v-model="date"
+                            label="Date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker
+                        v-model="date"
+                        no-title
+                        scrollable
+                        @input="menu=false"
+                        ></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="6" class="ma-0 pa-0">
+                    <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                            class="ma-0 pa-0"
+                            v-model="time"
+                            label="Time"
+                            prepend-icon="mdi-clock-time-four-outline"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                        ></v-text-field>
+                        </template>
+                        <v-time-picker
+                        v-model="time"
+                        full-width
+                        @input="menu2=false"
+                        ></v-time-picker>
+                    </v-menu>
+                </v-col>
+            </v-row>
+            <div style="width:100px;" class="mx-auto">
                 <v-btn rounded color="primary" @click="processAppt()">
                     BOOK NOW 
                 </v-btn>
